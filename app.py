409283a95895e23082ba1c0f84a73c58f5045d72
Felipe Monteiro Melo <monteirofelipep2@gmail.com>
2026-07-15 11:07:39 -0300

    finalmente

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 from controller import controller
 
-controller.iniciar()
+st.title("Controle de Gastos")
+
+st.write("Bem-vindo ao sistema!")
+
+if "tela" not in st.session_state:
+    st.session_state.tela = "iniciar"
